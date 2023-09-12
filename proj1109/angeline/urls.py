1765453fd1bed997e4c3bf7e71bebe0212de903b
pcadmin <bmartinsfontaina.com>
2023-09-12 16:33:13 -0300

    templates/angeline

--- proj1109/angeline/urls.py
+++ proj1109/angeline/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path, include
-from angeline.views import index
+from django.urls import path 
+from angeline.views import index, ex002
 
 urlpatterns ={
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('ex002', ex002, name='ex002'),
 }
--- proj1109/angeline/views.py
+++ proj1109/angeline/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def index (request):
     return HttpResponse("Olá, mundo!")
+
+def ex002(request):
+    context = {
+        'minha_string': 'Olá, Mundo!',
+        'meu_inteiro': 123,
+        'meu_booleano': True
+    }
+    return render(request, 'angeline/ex002.html', context)
