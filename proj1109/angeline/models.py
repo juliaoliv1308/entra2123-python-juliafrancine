57886b93a18386fec463420fcc90e1a3ae7745dd
Arthur H. Erhardt <juliaoliveirafrancine@gmail.com>
2023-11-01 16:13:32 -0300

    dump e loaddata

--- proj1109/angeline/models.py
+++ proj1109/angeline/models.py
@@ -1,32 +1,26 @@
 from django.db import models
+from datetime import datetime 
 
-class Pessoa(models.Model):
-    nome = models.CharField(max_length=100)
-    idade = models.PositiveIntegerField()
+class Cidade(models.Model):
     id = models.AutoField(primary_key=True)
-    cpf = models.CharField(max_length=11, blank=True)
-    rg = models.CharField(max_length=11, blank=True, default="999")
-    last_name = models.CharField(max_length=255, blank=True, default="")
-    email = models.EmailField(blank=True, default="", null=True)
-
-
-    ESTADO_CIVIL_CHOICES = [
-        ('CS', 'Casado'),
-        ('VD', 'Viuvo'),
-        ('SE', 'Separado'),
-        ('UN', 'União Estável'),
-        ('OU', 'Outros'),
-        ('', 'Nenhum'),
-    ]
-    estado_civil = models.CharField(
-      max_length=2,
-      choices=ESTADO_CIVIL_CHOICES,
-      default="",
-      blank=True
-    )
+    nome = models.CharField(max_length=255)
+    estado = models.CharField(max_length=255)
+    pais = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome
+
+class Pessoa(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Nome = models.CharField(max_length=255, default="Nome Padrão")
+    Sobrenome = models.CharField(max_length=255, default="Sobrenome Padrão")
+    Data_nascimento = models.DateField(null=True, default=datetime.now())
+    endereco = models.TextField(default="")
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE, default= "")
+
+    def __str__(self):
+        return f"{self.Nome} {self.Sobrenome}"
+
 
 
 
