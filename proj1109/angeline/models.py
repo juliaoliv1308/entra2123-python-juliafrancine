12bd40fc6a603bda2d8b3baaab7adcd24c855d01
Arthur H. Erhardt <juliaoliveirafrancine@gmail.com>
2023-11-01 16:38:26 -0300

    atividade

--- proj1109/angeline/models.py
+++ proj1109/angeline/models.py
@@ -1,26 +1,58 @@
 from django.db import models
-from datetime import datetime 
 
-class Cidade(models.Model):
+class LivroAutoAjuda(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=255)
-    estado = models.CharField(max_length=255)
-    pais = models.CharField(max_length=255)
+    resumo = models.TextField()
+    linkimg = models.ImageField(upload_to='livros/autoajuda/')
+    pdf = models.FileField(upload_to='livros/autoajuda/')
+    nota = models.DecimalField(max_digits=3, decimal_places=1)
+    sub_categoria = models.CharField(max_length=255, blank=True)
+    estoque = models.PositiveIntegerField()
+    categoria = models.PositiveIntegerField()
 
     def __str__(self):
         return self.nome
 
-class Pessoa(models.Model):
-    Id = models.AutoField(primary_key=True)
-    Nome = models.CharField(max_length=255, default="Nome Padrão")
-    Sobrenome = models.CharField(max_length=255, default="Sobrenome Padrão")
-    Data_nascimento = models.DateField(null=True, default=datetime.now())
-    endereco = models.TextField(default="")
-    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE, default= "")
+class LivroSuspense(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    resumo = models.TextField()
+    linkimg = models.ImageField(upload_to='livros/suspense/')
+    pdf = models.FileField(upload_to='livros/suspense/')
+    nota = models.DecimalField(max_digits=3, decimal_places=1)
+    sub_categoria = models.CharField(max_length=255, blank=True)  # Pode ser em branco
+    estoque = models.PositiveIntegerField()
+    categoria = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.Nome} {self.Sobrenome}"
+        return self.nome
 
+class LivroRomance(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    resumo = models.TextField()
+    linkimg = models.ImageField(upload_to='livros/romance/')
+    pdf = models.FileField(upload_to='livros/romance/')
+    nota = models.FloatField()
+    sub_categoria = models.CharField(max_length=50, blank=True)
+    estoque = models.IntegerField()
+    categoria = models.IntegerField()
+
+
+class LivroBiografia(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    resumo = models.TextField()
+    linkimg = models.CharField(max_length=255)
+    pdf = models.CharField(max_length=255)
+    nota = models.DecimalField(max_digits=3, decimal_places=1)
+    sub_categoria = models.CharField(max_length=255)
+    estoque = models.IntegerField()
+    categoria = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
 
 
 
